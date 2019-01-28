b4643a763821bf7b93ba7c8cb0d98213a31c3452
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-01-28 20:06:02 +0100

    Do not destroy one side

--- Assets/BLINDED_AM_ME package/Scripts/ExampleUseof_MeshCut.cs
+++ Assets/BLINDED_AM_ME package/Scripts/ExampleUseof_MeshCut.cs
@@ -7,30 +7,19 @@ public class ExampleUseof_MeshCut : MonoBehaviour
 
     void Update()
     {
-
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-
             if (Physics.Raycast(transform.position, transform.forward, out hit))
             {
-
                 GameObject victim = hit.collider.gameObject;
-
                 GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial);
-
-                if (!pieces[1].GetComponent<Rigidbody>())
-                    pieces[1].AddComponent<Rigidbody>();
-
-                Destroy(pieces[1], 1);
             }
-
         }
     }
 
     void OnDrawGizmosSelected()
     {
-
         Gizmos.color = Color.green;
 
         Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
@@ -39,7 +28,6 @@ public class ExampleUseof_MeshCut : MonoBehaviour
 
         Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.5f);
         Gizmos.DrawLine(transform.position, transform.position + -transform.up * 0.5f);
-
     }
 
 }
