4e53ae9eef78387c20d200a33ac034864f2b1097
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-01-30 18:50:22 +0100

    Add camera movement

---
+++ Assets/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public float speed = 100;
+
+    private const float MAX_VERTICAL_ROTATION = 90;
+    private float verticalRotation = -10;
+
+	// Use this for initialization
+	void Start () {
+        transform.RotateAround(Vector3.zero, Vector3.left, -10);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float horizontal = -(Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+        float vertical = -(Input.GetAxis("Vertical") * speed * Time.deltaTime);
+
+        transform.RotateAround(Vector3.zero, Vector3.up, horizontal);
+
+        if (Input.GetButton("Jump"))
+        {
+            //TODO: Fix
+            transform.position += transform.forward * vertical;
+            return;
+        }
+
+        if (MAX_VERTICAL_ROTATION < Mathf.Abs(verticalRotation + vertical))
+        {
+            return;
+        }
+
+        transform.RotateAround(Vector3.zero, -transform.right, vertical);
+        verticalRotation += vertical;
+    }
+}
