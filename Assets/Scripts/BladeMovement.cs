652d1511417d9842467b95640f08c74d5ccdabc7
Patrick Fehling <19287010+pa-ling@users.noreply.github.com>
2019-03-13 17:45:10 +0100

    Refactor BladeMovement

--- Assets/Scripts/BladeMovement.cs
+++ Assets/Scripts/BladeMovement.cs
@@ -5,9 +5,6 @@ using Leap.Unity.Interaction;
 
 public class BladeMovement : MonoBehaviour {
 
-    private enum HandObject { Laser, Marker};
-    private enum HandVector { PaperHit };
-
     private const int PAPER_LAYER_MASK = ~(1 << 2);
 
     public GameObject laserPrefab;
@@ -18,38 +15,15 @@ public class BladeMovement : MonoBehaviour {
     public HandModel rightHandModel;
     public InteractionHand rightInteractionHand;
 
-    private List<GameObject>[] handObjects;
-    private List<Vector3>[] handVectors;
     private GameObject verticalLaser;
 
     private void Start()
-    {
-        handObjects = new List<GameObject>[2]
-        {
-            new List<GameObject> { Instantiate(laserPrefab), Instantiate(markerPrefab) , null},
-            new List<GameObject> { Instantiate(laserPrefab), Instantiate(markerPrefab) , null},
-        };
-        handObjects[0][0].name = "Left Hand Laser";
-        handObjects[0][1].name = "Left Hand Marker";
-        handObjects[1][0].name = "Right Hand Laser";
-        handObjects[1][1].name = "Right Hand Marker";
-
-        handVectors = new List<Vector3>[2]
-        {
-            new List<Vector3>{ Vector3.negativeInfinity },
-            new List<Vector3>{ Vector3.negativeInfinity }
-        };
-
+    { 
         verticalLaser = Instantiate(laserPrefab);
         verticalLaser.name = "Vertical Laser";
 
-        leftInteractionHand.OnStayPrimaryHoveringObject += OnLeftPrimaryHover;
-        leftInteractionHand.OnEndPrimaryHoveringObject += OnLeftEndPrimaryHover;
-        leftInteractionHand.OnGraspBegin += OnLeftGraspBegin;
-
-        rightInteractionHand.OnStayPrimaryHoveringObject += OnRightPrimaryHover;
-        rightInteractionHand.OnEndPrimaryHoveringObject += OnRightEndPrimaryHover;
-        rightInteractionHand.OnGraspBegin += OnRightGraspBegin;
+        leftInteractionHand.OnGraspBegin += OnGraspBegin;
+        rightInteractionHand.OnGraspBegin += OnGraspBegin;
     }
 
     private void Update()
@@ -120,47 +94,7 @@ public class BladeMovement : MonoBehaviour {
         Debug.DrawRay(origin, Vector3.Normalize(destination - origin) * Vector3.Distance(origin, destination), color, 0, true);
     }
 
-    private void OnPrimaryHover(HandModel hand, InteractionHand interHand, InteractionBehaviour obj)
-    {
-        /*int handedness = (int) hand.Handedness;
-        Vector3 junction = Vector3.negativeInfinity;
-
-        GameObject marker = handObjects[handedness][(int)HandObject.Marker];
-
-        RaycastHit hit;
-        Vector3 thumbTipPos = hand.fingers[0].GetTipPosition();
-        Vector3 indexTipPos = hand.fingers[1].GetTipPosition();
-
-        if (interHand.isGraspingObject)
-        {
-            Vector3 graspPoint = interHand.GetGraspPoint();
-            graspPoint.y = obj.transform.position.y;
-            marker.transform.position = graspPoint;
-            marker.transform.rotation = obj.transform.rotation;
-            junction = graspPoint;
-            marker.SetActive(true);
-        } 
-        else if (Physics.Raycast(indexTipPos, thumbTipPos - indexTipPos, out hit, Vector3.Distance(indexTipPos, thumbTipPos), PAPER_LAYER_MASK))
-        {
-            marker.transform.position = hit.point;
-            marker.transform.rotation = obj.transform.rotation;
-            junction = hit.point;
-            marker.SetActive(true);
-        }
-        else
-        {
-            marker.SetActive(false);
-        }
-        
-        handVectors[handedness][(int)HandVector.PaperHit] = junction;*/
-    }
-
-    private void OnEndPrimaryHover(HandModel hand, InteractionBehaviour obj)
-    {
-        handVectors[(int)hand.Handedness][(int)HandVector.PaperHit] = Vector3.negativeInfinity;
-    }
-
-    private void OnGraspBegin(HandModel hand)
+    private void OnGraspBegin()
     {
         // when both hands grasp, the victim is cut
         if (leftInteractionHand.isGraspingObject && rightInteractionHand.isGraspingObject) //TODO: Make sure both hands are grasping the same object?
@@ -198,46 +132,6 @@ public class BladeMovement : MonoBehaviour {
             cutMarker.name = "Cut";*/
         }
     }
-
-    #region leftEventHandlers
-
-    private void OnLeftPrimaryHover(InteractionBehaviour obj)
-    {
-        OnPrimaryHover(leftHandModel, leftInteractionHand, obj);
-    }
-
-    private void OnLeftEndPrimaryHover(InteractionBehaviour obj)
-    {
-        OnEndPrimaryHover(leftHandModel, obj);
-    }
-
-    private void OnLeftGraspBegin()
-    {
-        Debug.Log("Left Grasp");
-        OnGraspBegin(leftHandModel);
-    }
-
-    #endregion leftEventHandlers
-
-    #region rightEventHandlers
-
-    private void OnRightPrimaryHover(InteractionBehaviour obj)
-    {
-        OnPrimaryHover(rightHandModel, rightInteractionHand, obj);
-    }
-
-    private void OnRightEndPrimaryHover(InteractionBehaviour obj)
-    {
-        OnEndPrimaryHover(rightHandModel, obj);
-    }
-
-    private void OnRightGraspBegin()
-    {
-        Debug.Log("Right Grasp");
-        OnGraspBegin(rightHandModel);
-    }
-
-    #endregion rightEventHandlers
 
     private void OnDrawGizmos()
     {
